dc80eca9a26b3b9e2d2bd124ea542ac0dd2335f8
José Ángel López <jle1001@alu.ubu.es>
2023-04-26 09:46:25 +0200

    [S6] add music player

--- app/__init__.py
+++ app/__init__.py
@@ -14,20 +14,28 @@ def upload_file():
     if request.method == 'POST':
         track = request.files['upload']
         track.save(f'./app/upload/upload{os.path.splitext(track.filename)[1]}')
-
-    waveform_img_src = plot_features.show_waveform()
-    spectogram_img_src = plot_features.show_spectogram()
-    chromagram_img_src = plot_features.show_chromagram()
-    MFCC_img_src = plot_features.show_MFCC()
-
-    prediction = predict_genre.predict()
     
-    return render_template('index.html', 
-                           waveform_image=waveform_img_src,
-                           spectogram_image=spectogram_img_src,
-                           chromagram_image=chromagram_img_src,
-                           MFCC_image=MFCC_img_src,
-                           prediction=prediction)
+    return render_template('upload.html')
+
+# @app.route('/analysis', methods=['POST', 'GET'])
+# def analysis():
+#     if request.method == 'POST':
+#         track = request.files['upload']
+#         track.save(f'./app/upload/upload{os.path.splitext(track.filename)[1]}')
+
+#     waveform_img_src = plot_features.show_waveform()
+#     spectogram_img_src = plot_features.show_spectogram()
+#     chromagram_img_src = plot_features.show_chromagram()
+#     MFCC_img_src = plot_features.show_MFCC()
+
+#     prediction = predict_genre.predict()
+    
+#     return render_template('analysis.html', 
+#                            waveform_image=waveform_img_src,
+#                            spectogram_image=spectogram_img_src,
+#                            chromagram_image=chromagram_img_src,
+#                            MFCC_image=MFCC_img_src,
+#                            prediction=prediction)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug=True)
