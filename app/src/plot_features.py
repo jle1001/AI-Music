d25396604ede8f8fad936845b51ce42e0c657408
José Ángel López <jle1001@alu.ubu.es>
2023-06-14 23:54:59 +0200

    [S12] update features representation design

--- app/src/plot_features.py
+++ app/src/plot_features.py
@@ -16,12 +16,13 @@ def show_waveform(audio=UPLOAD_TRACK):
         str: Base64-encoded image data in PNG format.
     """
     y, sr = librosa.load(audio)
-    fig = plt.figure(figsize=(12, 4))
+    fig = plt.figure(figsize=(12, 4), dpi=400)
     librosa.display.waveshow(y=y, sr=sr)
     plt.xlabel('Time (s)')
     plt.ylabel('Amplitude')
+    plt.subplots_adjust(left=0.10, right=0.95, bottom=0.15, top=0.90)
     buf = io.BytesIO()
-    fig.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
+    fig.savefig(buf, format='png')
     plt.close(fig)
     buf.seek(0)
     data = base64.b64encode(buf.read()).decode('ascii')
@@ -35,12 +36,13 @@ def show_spectogram(audio=UPLOAD_TRACK):
         str: Base64-encoded image data in PNG format.
     """
     y, sr = librosa.load(audio)
-    fig = plt.figure(figsize=(12, 4))
+    fig = plt.figure(figsize=(12, 4), dpi=400)
     spec = librosa.feature.melspectrogram(y=y, sr=sr)
     spec_db = librosa.power_to_db(spec, ref=np.max)
     img = librosa.display.specshow(spec_db, x_axis='time', y_axis='mel', sr=sr)
+    plt.subplots_adjust(left=0.10, right=0.95, bottom=0.15, top=0.90)
     buf = io.BytesIO()
-    fig.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
+    fig.savefig(buf, format='png')
     plt.close(fig)
     buf.seek(0)
     data = base64.b64encode(buf.read()).decode('ascii')
@@ -54,11 +56,12 @@ def show_chromagram(audio=UPLOAD_TRACK):
         str: Base64-encoded image data in PNG format.
     """
     y, sr = librosa.load(audio)
-    fig = plt.figure(figsize=(12, 4))
+    fig = plt.figure(figsize=(12, 4), dpi=400)
     chroma = librosa.feature.chroma_stft(y=y, sr=sr)
     img = librosa.display.specshow(chroma, y_axis='chroma', x_axis='time', cmap='coolwarm')
+    plt.subplots_adjust(left=0.10, right=0.95, bottom=0.15, top=0.90)
     buf = io.BytesIO()
-    fig.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
+    fig.savefig(buf, format='png')
     plt.close(fig)
     buf.seek(0)
     data = base64.b64encode(buf.read()).decode('ascii')
@@ -72,11 +75,12 @@ def show_MFCC(audio=UPLOAD_TRACK):
         str: Base64-encoded image data in PNG format.
     """
     y, sr = librosa.load(audio)
-    fig = plt.figure(figsize=(12, 4))
+    fig = plt.figure(figsize=(12, 4), dpi=400)
     mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20)
     img = librosa.display.specshow(mfccs, x_axis='time')
+    plt.subplots_adjust(left=0.10, right=0.95, bottom=0.15, top=0.90)
     buf = io.BytesIO()
-    fig.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
+    fig.savefig(buf, format='png')
     plt.close(fig)
     buf.seek(0)
     data = base64.b64encode(buf.read()).decode('ascii')
